4e737bf4cb944bef7140115dd4f48ef211730a9d
proninegor <118684723+borabora123@users.noreply.github.com>
2025-03-13 18:21:49 +0300

    classification

--- __init__.py
+++
@@ -1,9 +0,0 @@
-import torch
-import pandas as pd
-print(torch.cuda.is_available())
-print(torch.__version__)
-submit = pd.read_csv('/submit_res_net_50_64_42.csv')
-submit['ID'] = submit['patient_id'].astype(str)+'_'+submit['joint_id'].astype(str)
-submit['PAD'] = 0
-print(submit)
-submit.to_csv('C:\\Users\\User\\PycharmProjects\\ARISE\\submit.csv', index=False)
---
+++ utils.py
@@ -0,0 +1,17 @@
+import torch
+import os
+from hydra.utils import instantiate
+
+def save_checkpoint(model, epoch, jsn_accuracy, erosion_accuracy, save_dir):
+    torch.save(
+        model.state_dict(),
+        os.path.join(save_dir, f"model_epoch_{epoch}_jsn_accuracy_{jsn_accuracy}_erosion_accuracy_{erosion_accuracy}.pth")
+    )
+
+
+def load_eval_model(cfg, model_weights_path):
+    model = instantiate(cfg.model)
+    model.load_state_dict(torch.load(model_weights_path))
+    model.eval()
+    return model
+
